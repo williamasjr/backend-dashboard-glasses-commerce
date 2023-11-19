cb072f9878344a6d4e4f32ab70caa6fadd09083b
williamasjr <wasjr00@gmail.com>
2023-11-19 14:04:33 -0300

    add category component

---
+++ src/app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { PlusIcon } from "lucide-react";
+
+import { Button } from "@/src/components/ui/button";
+import { Heading } from "@/src/components/ui/heading";
+import { Separator } from "@/src/components/ui/separator";
+import { CategoryColumn, columns } from "./columns";
+import { DataTable } from "@/src/components/ui/data-table";
+import ApiList from "@/src/components/ui/api-list";
+
+interface CategoryClientProps {
+  data: CategoryColumn[];
+}
+
+export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Categories (${data.length})`}
+          description="Manage categories for you store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <PlusIcon className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API call for Categories" />
+      <Separator />
+      <ApiList entityName="categories" entityIdName="categoryId" />
+    </>
+  );
+};
--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -24,6 +24,12 @@ export function MainNav({
     },
 
     {
+      href: `/${params.storeId}/categories`,
+      label: "Categories",
+      active: pathname === `/${params.storeId}/categories`,
+    },
+
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
